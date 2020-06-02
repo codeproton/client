9f7e2a9160651cbbbf4841c4ed1b0fb0baf08976
zkingboos <kingboosoficial@gmail.com>
2020-06-01 21:16:15 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>Your mother is mine</p>
+    <head></head>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
